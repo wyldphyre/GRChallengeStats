64d065c8d18ff39cdf02c96dbdeb628ca461a6dd
Craig Reynolds <wyldphyre@gmail.com>
2020-06-21 14:55:52 +0800

    Introduce an invertable BooleanToVisibilityConverter
    The one built into the framework can't be reversed, so had to create one

---
+++ MvvmLibrary/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MvvmLibrary.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public bool Inverted { get; set; }
+
+        /// <summary>
+        /// Convert bool or Nullable bool to Visibility
+        /// </summary>
+        /// <param name="value">bool or Nullable bool</param>
+        /// <param name="targetType">Visibility</param>
+        /// <param name="parameter">null</param>
+        /// <param name="culture">null</param>
+        /// <returns>Visible or Collapsed</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = false;
+            if (value is bool boolean)
+            {
+                bValue = boolean;
+            }
+            else if (value is Nullable<bool>)
+            {
+                Nullable<bool> tmp = (Nullable<bool>)value;
+                bValue = tmp ?? false;
+            }
+
+            if (Inverted)
+            {
+                bValue = !bValue;
+            }
+
+            return (bValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Convert Visibility to boolean
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns>True or False</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility visibility ? (visibility == Visibility.Visible) && !Inverted : (object)false;
+        }
+    }
+}
